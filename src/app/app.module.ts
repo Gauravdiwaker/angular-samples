5bf68e795869859ccde072ed32521d80f1c8c708
gaurav <gauravdiwaker619@gmail.com>
2021-06-22 10:57:31 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
-import { NgIf } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { NgIfModule } from './ng-if/ng-if.module';
 import { NgSwitchModule } from './ng-switch/ng-switch.module';
 
 @NgModule({
@@ -16,7 +13,6 @@ import { NgSwitchModule } from './ng-switch/ng-switch.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgIfModule,
     NgSwitchModule
   ],
   providers: [],
